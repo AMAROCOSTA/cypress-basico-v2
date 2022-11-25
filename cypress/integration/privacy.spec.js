c5414b5f0b0c73b5647e187657a426a430ba9d3c
Amaro Rodrigues Ribeiro Costa <arribeiro@minsait.com>
2022-11-25 08:58:12 -0300

    atualização codigo

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,6 +1,8 @@
-it.only('testa a página da politica de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html')
-
-    cy.contains('Talking About Testing').should('be.visible')
+Cypress._.times(100, function(){
+    it('testa a página da politica de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
     
-    }) 
+        cy.contains('Talking About Testing').should('be.visible')
+        
+        }) 
+})
